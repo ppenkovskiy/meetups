aaf74e990bacf17d336eb36750e1965b491da897
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-04-07 22:57:54 +0300

    f_235

--- my_site/meetups/urls.py
+++ my_site/meetups/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('meetups/', views.index, name='all-meetups'),
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-details'),
 ]
--- my_site/meetups/views.py
+++ my_site/meetups/views.py
@@ -4,10 +4,33 @@ from django.http import HttpResponse
 
 def index(request):
     meetups = [
-        {'title': 'Meetup_1'},
-        {'title': 'Meetup_2'},
+        {
+            'title': 'Meetup_1',
+            'location': 'Location_1',
+            'slug': 'meetup-1'},
+        {
+            'title': 'Meetup_2',
+            'location': 'Location_2',
+            'slug': 'meetup-2'},
     ]
+
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
         'meetups': meetups
+    })
+
+
+def meetup_details(request, meetup_slug):
+    print(meetup_slug)
+    selected_meetup = {
+        'title': 'Meetup_1',
+        'description': 'Description_1',
+        'location': 'Location_1',
+        'address': 'Address_1',
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description'],
+        'meetup_location': selected_meetup['location'],
+        'meetup_address': selected_meetup['address'],
     })
