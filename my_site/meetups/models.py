c655d48d6335a4f6c2580b91e00cf7abea79529f
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-04-08 17:55:14 +0300

    f_240

--- my_site/meetups/models.py
+++ my_site/meetups/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Meetup(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
--- my_site/meetups/views.py
+++ my_site/meetups/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Meetup
 
 
 def index(request):
-    meetups = [
-        {
-            'title': 'Meetup_1',
-            'location': 'Location_1',
-            'slug': 'meetup-1'},
-        {
-            'title': 'Meetup_2',
-            'location': 'Location_2',
-            'slug': 'meetup-2'},
-    ]
+    meetups = Meetup.objects.all()
 
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
@@ -21,16 +12,18 @@ def index(request):
 
 
 def meetup_details(request, meetup_slug):
-    print(meetup_slug)
-    selected_meetup = {
-        'title': 'Meetup_1',
-        'description': 'Description_1',
-        'location': 'Location_1',
-        'address': 'Address_1',
-    }
-    return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description'],
-        'meetup_location': selected_meetup['location'],
-        'meetup_address': selected_meetup['address'],
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description,
+            'meetup_location': 'Location',
+            'meetuo_address': 'Address',
+        })
+
+    except Exception as exc:
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False,
+        })
