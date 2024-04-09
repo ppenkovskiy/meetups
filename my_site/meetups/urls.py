e0842b4033b8cc458016145ca704f9286f55613e
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-04-09 13:41:21 +0300

    f_249

--- my_site/meetups/urls.py
+++ my_site/meetups/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='all-meetups'),
+    path('meetups/success', views.confirm_registration, name='confirm-registration'),
     path('<slug:meetup_slug>', views.meetup_details, name='meetup-details'),
 ]
